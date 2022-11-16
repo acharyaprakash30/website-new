6c31b144747e6e15e1a5d03ff75399fbd0e8f404
abhishek Adhikari <lucifer.abhi2222@gmail.com>
2022-11-16 16:39:44 +0545

    login-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import HeadPart from './components/HeadPart/HeadPart'
+import FootPart from './components/FootPart/FootPart'
+import RightPart from './components/RightPart/RightPart'
+import LeftPart from './components/LeftPart/LeftPart'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="white-container">
+        <div className="blue-container">
+          <div className="second-white-container">
+            <HeadPart />
+            <div className="body-container">
+            <LeftPart />
+            <RightPart />
+            </div>
+            <FootPart />
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/LeftPart/LeftPart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './lp.css'
+import Men from '../../images/man.jpg'
+
+
+const LeftPart = () => {
+  return (
+    <div className='left-main-container'>
+      <img src={Men} alt="hancy" />
+    </div>
+  )
+}
+
+export default LeftPart
